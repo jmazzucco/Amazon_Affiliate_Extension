990b4affe00b25d11fdc093671aad8623a770b2e
John Mazzucco <John.Mazzucco@gmail.com>
2015-11-30 20:22:39 -0500

    Remove unused file. Assign URL parameter to a variable

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        var portal = "foo"
+
         if( details.url == "http://www.amazon.com/" || details.url == "https://www.amazon.com/")
-            return {redirectUrl: "https://www.amazon.com/foo" };
+            return {redirectUrl: "https://www.amazon.com/" + portal };
     },
     {urls: ["*://www.amazon.com/*"]},
     ["blocking"]);
