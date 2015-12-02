eb3d7b394f4c532e3999531216428cefb586be2d
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-01 21:03:48 -0500

    Store user input using the Chrome Storage API, get stored objects and display them in a list, and clear all stored objects

--- popup.js
+++ popup.js
@@ -1,7 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('submit_btn');
-    link.addEventListener('click', function() {
-        document.getElementById('newData').innerHTML = 'html data';
+// 1. populate HTML list with arrays saved in local storage when document is loaded
 
-    });
-});
+window.onload = function() {
+	document.getElementById('save').onclick = function() {
+		var name = document.getElementById('name').value;
+		var link = document.getElementById('link').value;
+		var obj = {};
+		obj[name] = link;
+		chrome.storage.sync.set(obj);
+	};
+
+	document.getElementById('get').onclick = function() {
+			chrome.storage.sync.get(null, function(items) {
+		    var allKeys = Object.keys(items);
+		    // var myNode = document.getElementById("newData");
+				// console.log(myNode.firstChild);
+				// while (myNode.firstChild) {
+    // 			myNode.removeChild(myNode.firstChild);
+				// }
+
+		   	for (var i = 0; i < allKeys.length; i++){
+		   		var key = allKeys[i]
+		   		document.getElementById('list').innerHTML = "";
+	   	 		chrome.storage.sync.get(key, function(data){
+
+	   	 			for (var property in data){
+	   	 				var newLi = document.createElement("li");
+	   	 				var txt = document.createTextNode(property + " " + data[property]);
+	   	 				newLi.appendChild(txt);
+							document.getElementById('list').appendChild(newLi
+						)};
+
+
+						// console.log(data)
+					// Move this to an addElement function after you get it working
+
+	   	 			//
+
+
+	   	 		});
+	   	 	};
+			});
+	};
+
+	document.getElementById('clear').onclick = function() {
+		chrome.storage.sync.clear();
+	};
+};
