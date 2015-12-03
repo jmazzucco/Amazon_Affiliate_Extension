fc60cd3519a7caa73dda0e266266984356081709
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-02 19:20:15 -0500

    Removed 'Get Stored Lines' button. Display list item when popup loads. Refresh list when new item is saved.

--- popup.js
+++ popup.js
@@ -1,39 +1,43 @@
 window.onload = function() {
+	function loadList() {
+		chrome.storage.sync.get(null, function(items) {
+	    var allKeys = Object.keys(items);
+
+	   	for (var i = 0; i < allKeys.length; i++){
+	   		var key = allKeys[i]
+
+	   		document.getElementById('list').innerHTML = "";
+	 	 		chrome.storage.sync.get(key, function(data){
+
+	 	 			for (var property in data){
+	 	 				var newLi = document.createElement("li");
+	 	 				newLi.setAttribute("id", property);
+	 	 				var br = document.createElement("br");
+	 	 				var name = document.createTextNode("Name: " + property);
+	 	 				var link = document.createTextNode("Link: " + data[property]);
+
+	 	 				newLi.appendChild(name);
+						document.getElementById('list').appendChild(newLi)
+
+						var newLi = document.createElement("li");
+						newLi.appendChild(link);
+						document.getElementById('list').appendChild(newLi)
+						document.getElementById('list').appendChild(br)
+					};
+	 	 		});
+	 	 	};
+		});
+	}
+
+	loadList();
+
 	document.getElementById('save').onclick = function() {
 		var name = document.getElementById('name').value;
 		var link = document.getElementById('link').value;
 		var obj = {};
 		obj[name] = link;
 		chrome.storage.sync.set(obj);
-	};
-
-	document.getElementById('get').onclick = function() {
-			chrome.storage.sync.get(null, function(items) {
-		    var allKeys = Object.keys(items);
-
-		   	for (var i = 0; i < allKeys.length; i++){
-		   		var key = allKeys[i]
-
-		   		document.getElementById('list').innerHTML = "";
-	   	 		chrome.storage.sync.get(key, function(data){
-
-	   	 			for (var property in data){
-	   	 				var newLi = document.createElement("li");
-	   	 				var br = document.createElement("br");
-	   	 				var name = document.createTextNode("Name: " + property);
-	   	 				var link = document.createTextNode("Link: " + data[property]);
-
-	   	 				newLi.appendChild(name);
-							document.getElementById('list').appendChild(newLi)
-
-							var newLi = document.createElement("li");
-							newLi.appendChild(link);
-							document.getElementById('list').appendChild(newLi)
-							document.getElementById('list').appendChild(br)
-						};
-	   	 		});
-	   	 	};
-			});
+		loadList()
 	};
 
 	document.getElementById('clear').onclick = function() {
