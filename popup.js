7d3cdbeac27447fc8bb0c35c235fec136308d09b
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-03 20:32:35 -0500

    Ignore Selected object when displaying list, rather than remove it. Add Checked HTML attribute to the selected radio list item

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
 	function loadList() {
 
 		chrome.storage.sync.get(null, function(items) {
-	    var allKeys = Object.keys(items);
 	    var allItems = items
-	    delete allItems["selected"]
+	    var selectedItem = allItems["selected"];
 
 		  if(allItems){
 		  	document.getElementById('radio_list').innerHTML = "";
 
 		   	for (var property in allItems){
-	 				//data[property] <- portal link
-	 				var newP = document.createElement("p");
-	 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label>"
-	 				document.getElementById('radio_list').appendChild(newP);
-	 				console.log(document.getElementById(property));
-	 				if (document.getElementById(property).id === "Test"){//Test is a place holder for the value found in the Selected object in storage
-	 					document.getElementById(property).setAttribute("checked", "checked");
-	 				};
+	 				//add all stored object keys to radio list, expect for the "selected" object key
+	 				if(!(property === "selected")) {
+		 				//data[property] <- portal link
+		 				var newP = document.createElement("p");
+		 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label>"
+		 				document.getElementById('radio_list').appendChild(newP);
+
+		 				if (document.getElementById(property).id === selectedItem){
+		 					document.getElementById(property).setAttribute("checked", "checked");
+		 				};
+		 			};
 		   	};
 			 };
 	   	listenForEvents();
@@ -28,9 +30,10 @@
 			var items = document.getElementsByClassName('items');
 			for(var i=0; i<items.length; i++)(function(i){
 			  items[i].onclick = function() {
-			  	console.log("click");
+			  	// console.log("click");
 			    var selectedObj = {};
-					selectedObj["selected"] = items[i];
+					selectedObj["selected"] = items[i].id;
+					// console.log(selectedObj);
 					chrome.storage.sync.set(selectedObj);
 			  }
 			})(i);
