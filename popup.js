ec77ef01281082a8e68c522daebf6b3a1a5effbb
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-03 20:47:02 -0500

    add comments

--- popup.js
+++ popup.js
@@ -10,10 +10,11 @@
 		   	for (var property in allItems){
 	 				//add all stored object keys to radio list, expect for the "selected" object key
 	 				if(!(property === "selected")) {
-		 				//data[property] <- portal link
 		 				var newP = document.createElement("p");
 		 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label>"
 		 				document.getElementById('radio_list').appendChild(newP);
+
+		 				//add Delete button next to each radio list item
 
 		 				if (document.getElementById(property).id === selectedItem){
 		 					document.getElementById(property).setAttribute("checked", "checked");
@@ -26,14 +27,11 @@
 	};
 
 	function listenForEvents(){
-			//check that loop doesn't run if items.length is 0
 			var items = document.getElementsByClassName('items');
 			for(var i=0; i<items.length; i++)(function(i){
 			  items[i].onclick = function() {
-			  	// console.log("click");
 			    var selectedObj = {};
 					selectedObj["selected"] = items[i].id;
-					// console.log(selectedObj);
 					chrome.storage.sync.set(selectedObj);
 			  }
 			})(i);
@@ -42,12 +40,16 @@
 				var name = document.getElementById('name').value;
 				var link = document.getElementById('link').value;
 				if(name && link){
+					//if storage is empty, create a new Selected object and add name as the value
 					var obj = {};
 					obj[name] = link;
 					chrome.storage.sync.set(obj);
 					loadList()
 				};
 			};
+
+			//add new function for deleting individual items from the list
+				//when the last list item is deleted, remove the Selected object as well
 
 			document.getElementById('clear').onclick = function() {
 				chrome.storage.sync.clear();
