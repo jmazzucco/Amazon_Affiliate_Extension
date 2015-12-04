2b34c6545b5589ee1f47bf359a961a19e1b343eb
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-03 19:18:58 -0500

    Save new object only if both Name and Link values are given. Remove the Selected object when displaying list. Update Selected object value when a radio button item is clicked

--- popup.js
+++ popup.js
@@ -2,38 +2,48 @@
 
 		chrome.storage.sync.get(null, function(items) {
 	    var allKeys = Object.keys(items);
-	    document.getElementById('radio_list').innerHTML = "";
-	   	for (var property in items){
-	 	 				//data[property] <- portal link
-	 	 				var newP = document.createElement("p");
-	 	 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label>"
-	 	 				document.getElementById('radio_list').appendChild(newP);
+	    var allItems = items
+	    delete allItems["selected"]
 
-	 	 				if (document.getElementById(property).id === "Test"){//Test is a place holder for the value found in the Selected object in storage
-	 	 					document.getElementById(property).setAttribute("checked", "checked");
-	 	 				};
-	   	};
+		  if(allItems){
+		  	document.getElementById('radio_list').innerHTML = "";
+
+		   	for (var property in allItems){
+	 				//data[property] <- portal link
+	 				var newP = document.createElement("p");
+	 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label>"
+	 				document.getElementById('radio_list').appendChild(newP);
+	 				console.log(document.getElementById(property));
+	 				if (document.getElementById(property).id === "Test"){//Test is a place holder for the value found in the Selected object in storage
+	 					document.getElementById(property).setAttribute("checked", "checked");
+	 				};
+		   	};
+			 };
 	   	listenForEvents();
 		});
 	};
 
-
 	function listenForEvents(){
-
+			//check that loop doesn't run if items.length is 0
 			var items = document.getElementsByClassName('items');
 			for(var i=0; i<items.length; i++)(function(i){
 			  items[i].onclick = function() {
-			      console.log("item was clicked!"+ i)
+			  	console.log("click");
+			    var selectedObj = {};
+					selectedObj["selected"] = items[i];
+					chrome.storage.sync.set(selectedObj);
 			  }
 			})(i);
 
 			document.getElementById('save').onclick = function() {
 				var name = document.getElementById('name').value;
 				var link = document.getElementById('link').value;
-				var obj = {};
-				obj[name] = link;
-				chrome.storage.sync.set(obj);
-				loadList()
+				if(name && link){
+					var obj = {};
+					obj[name] = link;
+					chrome.storage.sync.set(obj);
+					loadList()
+				};
 			};
 
 			document.getElementById('clear').onclick = function() {
