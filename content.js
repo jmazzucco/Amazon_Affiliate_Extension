304ca5d1561b9c7c9f68653da53f4d8b361a172e
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-08 10:07:18 -0500

    remove unnecessary code

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-   if (msg.param) {
-
-      window.location.href = window.location.href +msg.param;
-      msg.action = false;
-   };
+  if (msg.param) {
+    window.location.href = window.location.href + msg.param;
+  };
 });
