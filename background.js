e4dc2401e43d552a610b64a5fea1ad8db836d69f
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-08 10:03:08 -0500

    Add comments to background.js and convert tabs to spaces in Sublime

--- background.js
+++ background.js
@@ -1,78 +1,87 @@
-// get current param from storage
 var param;
 var changeUrl = false;
 var amazonStatus = false;
 
-var updatedListener = function(tabId,changeInfo,tab){
-    	//need to add: if url already contains a URL param but the param doesn't match the one being passed from addListener, remove the old param (everything proceeding the '?') and add the new one.
+  var updatedListener = function(tabId,changeInfo,tab){
+  //need to add: if url already contains a URL param but the param doesn't match the one being passed from addListener, remove the old param (everything proceeding the '?') and add the new one.
 
-    	if (changeInfo.url == "http://www.amazon.com/"){
-    		changeUrl = true;
-	    	amazonStatus = true;
+  //always add the parameter if the URL is "http://www.amazon.com/"
+  if (changeInfo.url == "http://www.amazon.com/"){
+    changeUrl = true;
+    amazonStatus = true;
 
-    	}else if((changeInfo.url) && (changeInfo.url.indexOf("www.amazon.com") > -1) && (amazonStatus === false)){
+  //if the previous URL did not contain "www.amazon.com", the param needs to be added
+  }else if((changeInfo.url) && (changeInfo.url.indexOf("www.amazon.com") > -1) && (amazonStatus === false)){
+      changeUrl = true;
+      amazonStatus = true;
 
-	    		changeUrl = true;
-	    		amazonStatus = true;
-	    } else if ((changeInfo.url) && (changeInfo.url.indexOf("www.amazon.com") == -1)){
-	    		amazonStatus = false;
-	    };
+  //if the previous URL did contain "www.amazon.com", the param does not need to be added
+  }else if ((changeInfo.url) && (changeInfo.url.indexOf("www.amazon.com") == -1)){
+      amazonStatus = false;
+  };
 
-	    if ((changeInfo.status == 'complete') && (changeUrl == true)) {
-	      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	         chrome.tabs.sendMessage(tabs[0].id, {param: param}, function(response) {
-	         });
-	      });
-	      changeUrl = false;
-			}
+  //send param to content.js if changeUrl is True and the page has finished loading
+  if ((changeInfo.status == 'complete') && (changeUrl == true)) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+       chrome.tabs.sendMessage(tabs[0].id, {param: param}, function(response) {
+       });
+    });
+    changeUrl = false;
+  }
 };
 
+//reload current tab
 var reload = function(addedTabId, removedTabId){
-		chrome.tabs.getSelected(null,function(tab) {
- 		if(tab.url.indexOf("www.amazon.com") > -1){
-	 		chrome.tabs.reload(addedTabId);
-	 		changeUrl = true;
-		  amazonStatus = true;
-		};
-	});
+  chrome.tabs.getSelected(null,function(tab) {
+    if(tab.url.indexOf("www.amazon.com") > -1){
+      chrome.tabs.reload(addedTabId);
+      changeUrl = true;
+      amazonStatus = true;
+    };
+  });
 };
 
+//when background.js loads, get the Affiliate Link of the selected object and pass it to the listeners
 function paramFromStorage(){
-    chrome.storage.sync.get(null, function(items) {
-	   	param = (items[items.selected]);
-	   	listeners(param);
-	  });
-
-	  newParam();
+  chrome.storage.sync.get(null, function(items) {
+    param = (items[items.selected]);
+    listeners(param);
+  });
+  newParam();
 };
 
+//when a new parameter is received from popup.js, pass it to the listeners
 function newParam(){
   chrome.runtime.onMessage.addListener(
-	  function(request, sender, sendResponse) {
-	     if (request.param != param){
-		   	param = request.param
-		    listeners(param);
-	     };
-	});
+    function(request, sender, sendResponse) {
+     if (request.param != param){
+      param = request.param
+      listeners(param);
+     };
+  });
 };
 
 function listeners(param){
-	function preloadCheck(){
-		chrome.tabs.onReplaced.addListener(reload);
-	};
+  //if Chrome prefetches resources when manually entering a different URL, reload the page so that URL change can be caught by the onUpdated listener
+  function preloadCheck(){
+    chrome.tabs.onReplaced.addListener(reload);
+  };
 
-	function addListener(param){
-		chrome.tabs.onUpdated.addListener(updatedListener);
-	};
+  //catch any changes made to the URL of the active tab
+  function addListener(param){
+    chrome.tabs.onUpdated.addListener(updatedListener);
+  };
 
-	function removeListeners(){
-		chrome.tabs.onUpdated.removeListener(updatedListener);
-		chrome.tabs.onReplaced.removeListener(reload);
-		preloadCheck();
-		addListener(param);
-	}
+  //remove all listeners (except for onMessage) and add them again using the new parameter
+  function removeListeners(){
+    chrome.tabs.onUpdated.removeListener(updatedListener);
+    chrome.tabs.onReplaced.removeListener(reload);
+    preloadCheck();
+    addListener(param);
+  }
 
-	removeListeners();
+  removeListeners();
 };
+
 
 paramFromStorage();
--- popup.js
+++ popup.js
@@ -1,92 +1,92 @@
-	function loadList() {
-		chrome.storage.sync.get(null, function(items) {
-	    var allItems = items;
+  function loadList() {
+    chrome.storage.sync.get(null, function(items) {
+      var allItems = items;
 
-	    //if Selected is the only object in storage, set it to "none"
-	    if (Object.keys(items).length === 1) {
-		 		var updateSelected = {};
-				updateSelected["selected"] = "none";
-				chrome.storage.sync.set(updateSelected);
-	  	};
+      //if Selected is the only object in storage, set it to "none"
+      if (Object.keys(items).length === 1) {
+        var updateSelected = {};
+        updateSelected["selected"] = "none";
+        chrome.storage.sync.set(updateSelected);
+      };
 
-	    var selectedItem = allItems["selected"];
+      var selectedItem = allItems["selected"];
 
-		  if(allItems){
-		  	document.getElementById('radio_list').innerHTML = "";
+      if(allItems){
+        document.getElementById('radio_list').innerHTML = "";
 
-		   	for (var property in allItems){
-	 				//add all stored object keys to radio list, expect for the "selected" object key
+        for (var property in allItems){
+          //add all stored object keys to radio list, expect for the "selected" object key
 
 
-	 				if(!(property === "selected")) {
+          if(!(property === "selected")) {
 
-	 					// assign Selected to key of list item if only one exists
-	 					if (allItems["selected"] === "none") {
-			    		var updateSelected = {};
-							updateSelected["selected"] = property;
-							allItems["selected"] = property;
-							chrome.storage.sync.set(updateSelected);
-				    };
+            // assign Selected to key of list item if only one exists
+            if (allItems["selected"] === "none") {
+              var updateSelected = {};
+              updateSelected["selected"] = property;
+              allItems["selected"] = property;
+              chrome.storage.sync.set(updateSelected);
+            };
 
-				    var selectedItem = allItems["selected"];
-		 				var newP = document.createElement("p");
-		 				newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label><button class='delete' id='"+property+"'>X</button>"
-		 				document.getElementById('radio_list').appendChild(newP);
+            var selectedItem = allItems["selected"];
+            var newP = document.createElement("p");
+            newP.innerHTML = "<input type='radio' class='items' name='radio_items' id='"+property+"'/><label for='radio_items'>"+property+"</label><button class='delete' id='"+property+"'>X</button>"
+            document.getElementById('radio_list').appendChild(newP);
 
-		 				if (document.getElementById(property).id === selectedItem){
-		 					document.getElementById(property).setAttribute("checked", "checked");
-		 				};
-		 			};
-		   	};
-			 };
+            if (document.getElementById(property).id === selectedItem){
+              document.getElementById(property).setAttribute("checked", "checked");
+            };
+          };
+        };
+       };
 
-			var selectedProperty = allItems["selected"];
-		  var urlParam = allItems[selectedProperty];
-		  // console.log(selecedParameter);
-	   	sendMessage(urlParam);
-	   	listenForEvents();
-		});
-	};
+      var selectedProperty = allItems["selected"];
+      var urlParam = allItems[selectedProperty];
+      // console.log(selecedParameter);
+      sendMessage(urlParam);
+      listenForEvents();
+    });
+  };
 
-	function sendMessage(urlParam){
-		chrome.runtime.sendMessage({param: urlParam});
-	};
+  function sendMessage(urlParam){
+    chrome.runtime.sendMessage({param: urlParam});
+  };
 
-	function listenForEvents(){
+  function listenForEvents(){
 
-			var items = document.getElementsByClassName('items');
-			for(var i=0; i<items.length; i++)(function(i){
-			  items[i].onclick = function() {
-			    var selectedObj = {};
-					selectedObj["selected"] = items[i].id;
-					chrome.storage.sync.set(selectedObj);
-					loadList();
-			  }
-			})(i);
+      var items = document.getElementsByClassName('items');
+      for(var i=0; i<items.length; i++)(function(i){
+        items[i].onclick = function() {
+          var selectedObj = {};
+          selectedObj["selected"] = items[i].id;
+          chrome.storage.sync.set(selectedObj);
+          loadList();
+        }
+      })(i);
 
-			var delete_btns = document.getElementsByClassName('delete');
-			for(var i=0; i<delete_btns.length; i++)(function(i){
-				delete_btns[i].onclick = function() {
-					var deleteName = delete_btns[i].id
+      var delete_btns = document.getElementsByClassName('delete');
+      for(var i=0; i<delete_btns.length; i++)(function(i){
+        delete_btns[i].onclick = function() {
+          var deleteName = delete_btns[i].id
 
-					chrome.storage.sync.get(null, function(items) {
-	    			var allItems = items;
-						chrome.storage.sync.remove(deleteName);
-						loadList();
-					});
-			  }
-			})(i);
+          chrome.storage.sync.get(null, function(items) {
+            var allItems = items;
+            chrome.storage.sync.remove(deleteName);
+            loadList();
+          });
+        }
+      })(i);
 
-			document.getElementById('save').onclick = function() {
-				var name = document.getElementById('name').value;
-				var link = document.getElementById('link').value;
-				if(name && link){
-					var obj = {};
-					obj[name] = link;
-					chrome.storage.sync.set(obj);
-					loadList();
-				};
-			};
-	};
+      document.getElementById('save').onclick = function() {
+        var name = document.getElementById('name').value;
+        var link = document.getElementById('link').value;
+        if(name && link){
+          var obj = {};
+          obj[name] = link;
+          chrome.storage.sync.set(obj);
+          loadList();
+        };
+      };
+  };
 
-	loadList();
+  loadList();
