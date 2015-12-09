8608cb80c977ac59f256dd077c3b3b29cc554410
John Mazzucco <John.Mazzucco@gmail.com>
2015-12-09 09:13:05 -0500

    add a '?' at the end of the current URL if it doesn't contain one

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.param) {
-    //append current URL with the param
+
+    //append current URL with symbols before adding the param
     url = window.location.href
     if(url.endsWith('amazon.com')){
     	url = url + '/?'
-    } else if (url.endsWith('amazon.com/')) {
-    	url = url + '?'
-    };
+    }
 
+    if(url.indexOf('?') == -1){
+    	url = url + "?"
+    }
+
+    //append current URL with the param
     window.location.href = url +'&'+ msg.param;
   };
 });
